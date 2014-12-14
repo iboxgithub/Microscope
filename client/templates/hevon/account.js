f0c683ee7dc2dfcb7ef1ab35f12f95b5ef7b3469
iboxgithub <ibox@laposte.net>
2014-12-14 16:59:07 +0100

    connection to Hevon Core to do

--- client/templates/hevon/account.js
+++ client/templates/hevon/account.js
@@ -3,8 +3,7 @@ Template.account.created = function() {
 
 Template.account.helpers({
     userTransacs: function() {
-        console.log('TEST  ' + userId);
-        return Transaction_user.find({id_user:'kWGZA7btmNq3AkN8B'/*this.currentUser*/});
+        return Transaction_user.find({id_user:Meteor.userId()});
     }
 });
 
--- client/templates/hevon/transfer.js
+++ client/templates/hevon/transfer.js
@@ -1,10 +1,56 @@
 Template.transfer.created = function() {
+    //Session.set('postSubmitErrors', {});
+
+    //TODO : get
+    // - the array of ccy/rate accepted
+    // - the array of client's reserves his different currencies
+    // - token (in the future for a secure OAuth access)
 };
 
 Template.transfer.helpers({
-
+    errorMessage: function(field) {
+        return Session.get('postSubmitErrors')[field];
+    },
+    errorClass: function (field) {
+        return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+    }
 });
 
 Template.transfer.events({
 
+    //"leaving" the amount input box
+    'blur .amount_from': function(e) {
+        e.preventDefault();
+
+        var amount = $(e.target).val();
+        console.log('hey : ' + amount);
+
+    },
+    'submit form': function(e) {
+        e.preventDefault();
+
+        var amount = $(e.target).find('[name=amount_from]').val();
+        console.log('hey : ' + amount);
+        console.log('sdf');
+        /*var post = {
+            url: $(e.target).find('[name=url]').val(),
+            title: $(e.target).find('[name=title]').val()
+        };
+
+        var errors = validatePost(post);
+        if (errors.title || errors.url)
+            return Session.set('postSubmitErrors', errors);
+
+        Meteor.call('postInsert', post, function(error, result) {
+            // display the error to the user and abort
+            if (error)
+                return throwError(error.reason);
+
+            // show this result but route anyway
+            if (result.postExists)
+                throwError('This link has already been posted');
+
+            Router.go('postPage', {_id: result._id});
+        });*/
+    }
 });
